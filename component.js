65dfc4658d1fdf629beb311b3eb66be0944604cc
marchuanv <marchuanv@gmail.com>
2021-02-24 13:42:47 +0200

    refactor

--- component.js
+++ component.js
@@ -1,16 +1,13 @@
-const path = require("path");
-const Module = require('module');
-const cacheRequire = Module.prototype.require;
-Module.prototype.require = (module, cache) => {
-    if (cache === undefined || cache === null || cache === true){
-        return cacheRequire(module);
-    } else {
-        delete require.cache[require.resolve(module)];
-        return require(module);
-    }
-}
 const cache = [];
 module.exports = {
+    require: (moduleName, cache = true) => {
+        if (cache){
+            return require(moduleName);
+        } else {
+            delete require.cache[require.resolve(moduleName)];
+            return require(moduleName);
+        }
+    },
     cache: {
         add: (name, value) => {
             cache.push({ name,value });
