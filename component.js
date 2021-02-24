795c393962fd4fa5c31fffed2023c9c22e4444a2
marchuanv <marchuanv@gmail.com>
2021-02-24 14:18:52 +0200

    refactor

--- component.js
+++ component.js
@@ -1,19 +1,24 @@
-const cache = [];
+const dependencies = [];
 module.exports = {
     require: (moduleName, cache = true) => {
+        let required = null;
         if (cache){
-            return require(moduleName);
+            required = require(moduleName);
         } else {
-            delete require.cache[require.resolve(moduleName)];
-            return require(moduleName);
+            const resolvedModuleName = require.resolve(moduleName);
+            if (resolvedModuleName){
+                delete require.cache[resolvedModuleName];
+                required = require(moduleName);
+            } else {
+                throw new Error(`could not resolve ${moduleName}`);
+            }
+        }
+        if (required){
+           // dependencies.push({ name: moduleName, dependency: dependantModuleName });
+            return required;
         }
     },
-    cache: {
-        add: (name, value) => {
-            cache.push({ name,value });
-        },
-        find: (name) => {
-            return cache.find( x => x.name === name );
-        }
+    getDependency: (moduleName) => {
+        return dependencies.find(d => d.name === moduleName).dependency;
     }
 };
--- test.js
+++ test.js
@@ -1,5 +1,10 @@
 const comonent = require("./component.js");
-(async() => { 
+(async() => {
+
+    const crypto = comonent.require("crypto", "Test");
+
+    const test = comonent.getDependency("crypto")
+
     require("./package.json",false);
     process.exit();
 })().catch((err)=>{
