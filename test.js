6ab92b889536ebda30167701fba0857e0fa16378
marchuanv <marchuanv@gmail.com>
2021-03-10 04:42:17 +0200

    fix

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ let com  = require("./component.js");
 (async() => {
     let { component } = await com.require("component", {gitUsername: "marchuanv"});
     await component.require("component.delegate", {gitUsername: "marchuanv"});
-    await component.register();
+    await component.register(module, "component");
 })().catch((err)=>{
     console.error(err);
 });
