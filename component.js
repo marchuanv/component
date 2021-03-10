fabfe4abc4204f1cad610e5527299ba164e74c51
marchuanv <marchuanv@gmail.com>
2021-03-10 12:52:30 +0200

    refactor

--- component.js
+++ component.js
@@ -67,50 +67,31 @@ module.exports = function(moduleName) {
     });
     this.require = ( moduleName, { gitUsername } ) => {
         return new Promise(async (resolve) => {
-            
-            let moduleToInstall =  moduleName;
-            
-            const installedNodeModules = []; 
-            let modulesDir = path.join(__dirname, "../");
-
-            if (modulesDir.indexOf("node_modules") === -1){
-                modulesDir = path.join(__dirname, "node_modules");
-            }
-           
-            fs.readdirSync(modulesDir).forEach(dirName => {
-                installedNodeModules.push(dirName);
-            });
-            
-            const canInstallModule = !installedNodeModules.find(modName => modName === moduleName);
-            if (canInstallModule){
+            let resolvedPath = require.resolve(moduleName);
+            if (!resolvedPath){
+                let moduleToInstall;
                 if (gitUsername) {
                     moduleToInstall = `${gitUsername}/${moduleName}`;
                 }
                 await installModule(moduleToInstall);
+                resolvedPath = require.resolve(moduleName);
             }
             let moduleResults = {};
-            const resolvedPath = require.resolve(moduleName);
-            if (resolvedPath){
-                const { name, hostname, port } = getPackage(moduleName, resolvedPath);
-                if (moduleName.startsWith("component")){
-                    moduleResults["hostname"] = hostname;
-                    moduleResults["port"] = port;
-                    moduleResults["name"] = name;
-                    if (!hostname || !port){
-                        throw new Error(`failed to register ${moduleName}, package.json requires hostname and port configuration`);
-                    }
+            const { name, hostname, port } = getPackage(moduleName, resolvedPath);
+            if (moduleName.startsWith("component")){
+                moduleResults["hostname"] = hostname;
+                moduleResults["port"] = port;
+                moduleResults["name"] = name;
+                if (!hostname || !port){
+                    throw new Error(`failed to register ${moduleName}, package.json requires hostname and port configuration`);
                 }
-                const requiredModule = require(moduleName);
-                moduleResults[formatModuleName(moduleName)] = requiredModule;
-                await resolve(moduleResults);
-
-                for(const knownComponent of knownComponents){
-                    await knownComponent.delegate.call( { name: "acquired" }, moduleResults );
-                };
-                
-            } else {
-                reject(new Error(`failed to register ${moduleName}, could not resolve ${moduleName}, see npm logs it might not be installed.`));
             }
+            const requiredModule = require(moduleName);
+            moduleResults[formatModuleName(moduleName)] = requiredModule;
+            await resolve(moduleResults);
+            for(const knownComponent of knownComponents){
+                await knownComponent.delegate.call( { name: "acquired" }, moduleResults );
+            };
         });
     };
 };
