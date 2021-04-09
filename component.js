d0f07b713a0313f4272cf2d10ef0bb1a4806064f
marchuanv <marchuanv@gmail.com>
2021-04-09 17:42:31 +0200

    refactor

--- component.js
+++ component.js
@@ -19,7 +19,7 @@ module.exports = {
             }
             module.exports.registry.push(registeredComponent);
         }
-        for(const { moduleName } of registeredComponent.config.publishers) {
+        for(const { moduleName } of registeredComponent.config.dependencies) {
             await module.exports.load(moduleName);
         };
         registeredComponent.exports = require(registeredComponent.config.resolvedPath);
